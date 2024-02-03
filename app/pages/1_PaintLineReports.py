932bb1671f9563e1178b5f1b234bbfd7b2af2464
AbhranilGit <abhranil.github@gmail.com>
2024-02-03 22:58:42 +0530

    New Changes

--- app/pages/1_PaintLineReports.py
+++ app/pages/1_PaintLineReports.py
@@ -37,6 +37,7 @@ def run_query(query,values):
         print()
     return result
 
+# @st.cache_data(ttl=1*24*60*60) # 1 day  
 def display_current_status():
     Status = 'Running'
     StopTime = 'NA'
@@ -70,7 +71,7 @@ def get_downtime_by_parameter(parameter,today_date):
     print()
     curr_qry = f"""with s1 as
                    (
-                     select date(stop_date) as stop_date, {parameter}, start_date - stop_date as duration 
+                     select date(stop_date) as stop_date, coalesce({parameter},'NA') as {parameter}, start_date - stop_date as duration 
                      from LineStopSchema.production_line_table plt 
                      where start_date is not null and stop_date::date > (current_date - interval '6 months')::date
                    )
@@ -78,6 +79,9 @@ def get_downtime_by_parameter(parameter,today_date):
                    from s1
                    group by GROUPING sets ((stop_date, {parameter}),(stop_date))
                    order by stop_date desc, {parameter}"""
+    # print("Query:")
+    # print(curr_qry)
+    # print()
     curr_values = {}
     db_res = run_query(curr_qry,curr_values)
     db_res_dict = {}
@@ -133,6 +137,26 @@ def convert_df_to_csv(df):
   # IMPORTANT: Cache the conversion to prevent computation on every rerun
   return df.to_csv(index=False).encode('utf-8')
 
+def format_cause_data(db_res_dict,start_date,stop_date):
+    cause_dict = {}
+    for stp_date, details_dict in db_res_dict.items():
+        if stp_date>=start_date and stp_date<=stop_date:
+            for cause,dur in details_dict.items():
+                cause = cause.strip().title()
+                if (cause.lower() != "all") and not(cause.lower().startswith('test')) and ('na' != cause.lower()):
+                    if not(cause in cause_dict):
+                        cause_dict[cause] = dur
+                    else:
+                        cause_dict[cause] = cause_dict[cause]+dur
+    resp_dict = {'Cause':[],'Duration':[]}
+    for cause,dur in cause_dict.items():
+        resp_dict['Cause'].append(cause)
+        resp_dict['Duration'].append(dur)
+    resp_df = pd.DataFrame.from_dict(resp_dict).sort_values(by='Duration',ascending=False).head(10).reset_index(drop=True)
+    resp_df['Duration'] = (resp_df['Duration']/60).astype(int)
+    return resp_df
+    
+
 st.set_page_config(
     page_title="Dunbarton",
     page_icon="🚪",
@@ -153,26 +177,42 @@ col1, col2, col3 = st.columns(3)
 with col1:
     cat_id = st.selectbox(label ="Category ID:",options=all_parameters,index=0)
 with col2:
-    start_date = st.date_input(label="Start Date:", value=min(all_dates), min_value=min(all_dates), max_value=all_dates[-2])
+    cat_id_start_date = st.date_input(label="Start Date:", value=min(all_dates), min_value=min(all_dates), max_value=all_dates[-2])
 with col3:
-    stop_date = st.date_input(label="Stop Date:", value = max(all_dates), min_value=all_dates[all_dates.index(start_date)+1], max_value=max(all_dates))
+    cat_id_stop_date = st.date_input(label="Stop Date:", value = max(all_dates), min_value=all_dates[all_dates.index(cat_id_start_date)+1], max_value=max(all_dates))
     
-DF = format_data(all_dates,all_parameters,db_res_dict,start_date,stop_date)
+CatID_DF = format_data(all_dates,all_parameters,db_res_dict,cat_id_start_date,cat_id_stop_date)
 
 but_col1, but_col1, but_col3 = st.columns([3, 2, 1])
 with but_col3:
     st.download_button(
     label="Download data as CSV",
-    data=convert_df_to_csv(DF),
+    data=convert_df_to_csv(CatID_DF),
     file_name='Report.csv',
     mime='text/csv',
     )
 
-chart = alt.Chart(DF).mark_bar().encode(
+chart = alt.Chart(CatID_DF).mark_bar().encode(
             x=alt.X('Date:O', axis=alt.Axis(labelOverlap="greedy",grid=False,labelAngle=-45,)),
             y=alt.Y(cat_id,title="Down Time (Mins)"))
 st.altair_chart(chart, use_container_width=True)
 st.divider()
 
+all_dates,all_parameters,db_res_dict = get_downtime_by_parameter("cause",curr_date)
+st.subheader(body = 'Paint Line Downtime By Causes (Top 10)', anchor='#paint-line-reports')
+
+col1, col2 = st.columns(2)
+with col1:
+    cause_start_date = st.date_input(label="Start Date:", value=min(all_dates), min_value=min(all_dates), max_value=all_dates[-2], key='cause_start_date')
+with col2:
+    cause_stop_date = st.date_input(label="Stop Date:", value = max(all_dates), min_value=all_dates[all_dates.index(cause_start_date)+1], max_value=max(all_dates), key='cause_stop_date')
+    
+CauseDF = format_cause_data(db_res_dict,cause_start_date,cause_stop_date)
+
+chart = alt.Chart(CauseDF).mark_bar().encode(
+            x=alt.X('Cause', axis=alt.Axis(labelOverlap="greedy",grid=False,labelAngle=-45),sort=None),
+            y=alt.Y('Duration',title="Down Time (Mins)"))
+st.altair_chart(chart, use_container_width=True)
+st.divider()
 
 
